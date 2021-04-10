03acd2492bf9c1bfe7b1e6293f16029061e6622c
viandwi24 <fiandwi0424@gmail.com>
2021-04-11 05:44:48 +0700

    add all transport nav

--- app/Http/Controllers/Api/NavigationController.php
+++ app/Http/Controllers/Api/NavigationController.php
@@ -8,6 +8,8 @@ use App\Models\Transportation;
 use App\Models\TransportationRoutes;
 use Illuminate\Support\Collection;
 
+use function PHPUnit\Framework\returnValue;
+
 class NavigationController extends Controller
 {
     public function vincentyGreatCircleDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000)
@@ -43,120 +45,84 @@ class NavigationController extends Controller
             'lng' => floatval($destinations[1]),
         ];
 
-        // transporations
-        // $transportations = Transportation::select('id', 'name', 'description')->get();
-        // $transportations->load('routes');
-        // $data['transportations'] = $transportations;
+        // get all prediction
+        $transportations = Transportation::with('routes')
+            ->select('id', 'name', 'description')
+            ->get();
+        
+        $result = [];
+        $bestScore = INF;
+        $bestScoreIndex = null;
+        for ($i=0; $i < count($transportations); $i++) { 
+            $tmp = $this->_getRouting($data['origin'], $data['destination'], $transportations[$i]);
+            if ($bestScore > $tmp['score']) {
+                $bestScoreIndex = $i;
+                $bestScore = $tmp['score'];
+            }
+            $result[] = $tmp;
+        }
 
         // 
-        $routes = TransportationRoutes::select('id', 'transportation_id', 'latitude', 'longitude')->get();
-        $routes = new Collection($routes);
-        $routes_list = $routes->map(function (TransportationRoutes $e) {
-            return ['id' => $e->id, 'transportation_id' => $e->transportation_id, 'lat' => $e->latitude, 'lng' => $e->longitude];
+        usort($result, function($first, $second){
+            return $first['score'] > $second['score'];
         });
 
+        // 
+        $data['best'] = $result[$bestScoreIndex];
+        $data['transportations'] = $result;
+        return $data;
+    }
+
+
+    private function _getRouting($origin, $destination, $transportation)
+    {
+        // 
+        // $transportation = $transportation->toArray
+        $routes = $transportation->routes;
+
+        // 
+        $data = $transportation->toArray();
 
         // get closed list from destination
         $closestDistance = INF;
         $closestPoint = null;
-        $zero = 0;
-        for($i = 0; $i < count($routes_list); $i++)
+        for($i = 0; $i < count($routes); $i++)
         {
-            $route = $routes_list[$i];
+            $route = $routes[$i];
             $distance = $this->vincentyGreatCircleDistance(
-                $route['lat'], $route['lng'],
-                $data['destination']['lat'], $data['destination']['lng'],
+                $route['latitude'], $route['longitude'],
+                $destination['lat'], $destination['lng'],
             );
             if ($distance < $closestDistance) {
                 $closestPoint = $route;
                 $closestDistance = $distance;
             }
         }
-        $data['closestPointFromDestination'] = $closestPoint; 
+        $data['closestPointFromDestination'] = $closestPoint;
+        $data['scoreDestination'] = $closestDistance;
 
-
-        // get list route from transporation
-        $routes = TransportationRoutes::select('id', 'transportation_id', 'latitude', 'longitude')
-            ->where('transportation_id', $data['closestPointFromDestination']['transportation_id'])
-            ->get();
-        $routes = new Collection($routes);
-        $routes_list = $routes->map(function (TransportationRoutes $e) {
-            return ['id' => $e->id, 'transportation_id' => $e->transportation_id, 'lat' => $e->latitude, 'lng' => $e->longitude];
-        });
 
         // get closed list from origin
         $closestDistance = INF;
         $closestPoint = null;
-        $zero = 0;
-        for($i = 0; $i < count($routes_list); $i++)
+        for($i = 0; $i < count($routes); $i++)
         {
-            $route = $routes_list[$i];
+            $route = $routes[$i];
             $distance = $this->vincentyGreatCircleDistance(
-                $route['lat'], $route['lng'],
-                $data['origin']['lat'], $data['origin']['lng'],
+                $route['latitude'], $route['longitude'],
+                $origin['lat'], $origin['lng'],
             );
             if ($distance < $closestDistance) {
                 $closestPoint = $route;
                 $closestDistance = $distance;
             }
         }
-        $data['closestPointFromOrigin'] = $closestPoint; 
+        $data['closestPointFromOrigin'] = $closestPoint;
+        $data['scoreOrigin'] = $closestDistance;
 
         // 
-        $data['transportation'] = Transportation::find($closestPoint['transportation_id']);
-        $data['transportation']->load('routes');
-
-        // 
-        // $data['routes'] = $routes; 
-        return $data;
-        // $routes = [
-        //     [
-        //         'lat' => -7.52222811929988,
-        //         'lng' => 112.41398625075817
-        //     ],
-        //     [
-        //         'lat' => -7.52222811929988,
-        //         'lng' => 112.41401977837086,
-        //     ],
-        //     [
-        //         'lat' => -7.522654908864785, 
-        //         'lng' => 112.41403721272945
-        //     ]
-        // ];
-        // $routes = [
-        //     [
-        //         'lat' => 2,
-        //         'lng' => 3
-        //     ],
-        //     [
-        //         'lat' => 5,
-        //         'lng' => 10
-        //     ],
-        //     [
-        //         'lat' => 4,
-        //         'lng' => 2
-        //     ]
-        // ];
-        // http://180.250.159.51:7777/api/navigation?origin=-7.522284,112.41350599999998&destination=-7.521199036192701,112.41389773786068
-
-        // 
-        $bestScore = INF;
-        $bestScoreIndex = 0;
-        for($i = 0; $i < count($routes); $i++)
-        {
-            $route = $routes[$i];
-            $routes[$i]['latMin'] = ($route['lat'] - $data['origin']['lat']);
-            $routes[$i]['lngMin'] = ($route['lng'] - $data['origin']['lng']);
-            $routes[$i]['score'] = abs($routes[$i]['latMin'] + $routes[$i]['lngMin']);
-            if ($routes[$i]['score'] < $bestScore) {
-                $bestScore = $routes[$i]['score'];
-                $bestScoreIndex = $i;
-            }
-        }
-
-        //
-        $data['bestCoordinate'] = $routes[$bestScoreIndex];
-        $data['routes'] = $routes; 
+        $data['score'] = $data['scoreOrigin'] + $data['scoreDestination'];
+        $data['routes'] = $transportation->routes;
         return $data;
     }
 }
