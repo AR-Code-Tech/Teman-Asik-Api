7c57788ec655bc2669c6ac2a8a94c1c928292299
viandwi24 <fiandwi0424@gmail.com>
2021-04-10 21:36:59 +0700

    add

--- app/Http/Controllers/Api/NavigationController.php
+++ app/Http/Controllers/Api/NavigationController.php
@@ -5,9 +5,26 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Transportation;
+use App\Models\TransportationRoutes;
+use Illuminate\Support\Collection;
 
 class NavigationController extends Controller
 {
+    public function vincentyGreatCircleDistance($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 6371000)
+    {
+        $latFrom = deg2rad($latitudeFrom);
+        $lonFrom = deg2rad($longitudeFrom);
+        $latTo = deg2rad($latitudeTo);
+        $lonTo = deg2rad($longitudeTo);
+        $lonDelta = $lonTo - $lonFrom;
+        $a = pow(cos($latTo) * sin($lonDelta), 2) +
+            pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+        $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+        
+        $angle = atan2(sqrt($a), $b);
+        return $angle * $earthRadius;
+    }
+
     public function index(Request $request)
     {
         $data = [];
@@ -32,21 +49,80 @@ class NavigationController extends Controller
         // $data['transportations'] = $transportations;
 
         // 
-        // $routes = 
-        $routes = [
-            [
-                'lat' => -7.52222811929988,
-                'lng' => 112.41398625075817
-            ],
-            [
-                'lat' => -7.52222811929988,
-                'lng' => 112.41401977837086,
-            ],
-            [
-                'lat' => -7.522654908864785, 
-                'lng' => 112.41403721272945
-            ]
-        ];
+        $routes = TransportationRoutes::select('id', 'transportation_id', 'latitude', 'longitude')->get();
+        $routes = new Collection($routes);
+        $routes_list = $routes->map(function (TransportationRoutes $e) {
+            return ['id' => $e->id, 'transportation_id' => $e->transportation_id, 'lat' => $e->latitude, 'lng' => $e->longitude];
+        });
+
+
+        // get closed list from destination
+        $closestDistance = INF;
+        $closestPoint = null;
+        $zero = 0;
+        for($i = 0; $i < count($routes_list); $i++)
+        {
+            $route = $routes_list[$i];
+            $distance = $this->vincentyGreatCircleDistance(
+                $route['lat'], $route['lng'],
+                $data['destination']['lat'], $data['destination']['lng'],
+            );
+            if ($distance < $closestDistance) {
+                $closestPoint = $route;
+                $closestDistance = $distance;
+            }
+        }
+        $data['closestPointFromDestination'] = $closestPoint; 
+
+
+        // get list route from transporation
+        $routes = TransportationRoutes::select('id', 'transportation_id', 'latitude', 'longitude')
+            ->where('transportation_id', $data['closestPointFromDestination']['transportation_id'])
+            ->get();
+        $routes = new Collection($routes);
+        $routes_list = $routes->map(function (TransportationRoutes $e) {
+            return ['id' => $e->id, 'transportation_id' => $e->transportation_id, 'lat' => $e->latitude, 'lng' => $e->longitude];
+        });
+
+        // get closed list from origin
+        $closestDistance = INF;
+        $closestPoint = null;
+        $zero = 0;
+        for($i = 0; $i < count($routes_list); $i++)
+        {
+            $route = $routes_list[$i];
+            $distance = $this->vincentyGreatCircleDistance(
+                $route['lat'], $route['lng'],
+                $data['origin']['lat'], $data['origin']['lng'],
+            );
+            if ($distance < $closestDistance) {
+                $closestPoint = $route;
+                $closestDistance = $distance;
+            }
+        }
+        $data['closestPointFromOrigin'] = $closestPoint; 
+
+        // 
+        $data['transportation'] = Transportation::find($closestPoint['transportation_id']);
+        $data['transportation']->load('routes');
+
+        // 
+        // $data['routes'] = $routes; 
+        return $data;
+        // $routes = [
+        //     [
+        //         'lat' => -7.52222811929988,
+        //         'lng' => 112.41398625075817
+        //     ],
+        //     [
+        //         'lat' => -7.52222811929988,
+        //         'lng' => 112.41401977837086,
+        //     ],
+        //     [
+        //         'lat' => -7.522654908864785, 
+        //         'lng' => 112.41403721272945
+        //     ]
+        // ];
         // $routes = [
         //     [
         //         'lat' => 2,
